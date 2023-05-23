312ce78f1d0171c1bc53e9bf5871fbdc367ba91f
Emre Bardakcı <103515994+EmreBardakci@users.noreply.github.com>
2023-05-23 15:18:38 +0300

    SinemaBiletRezervasyon

---
+++ script.js
@@ -0,0 +1,68 @@
+const container = document.querySelector(".container");
+const count = document.getElementById("count");
+const amount = document.getElementById("amount");
+const select = document.getElementById("movie");
+const seats = document.querySelectorAll(".seat:not(.reserved)");
+
+getFromLocalStorage();
+calculeteTotal();
+
+container.addEventListener("click", function (e) {
+  if (
+    e.target.classList.contains("seat") &&
+    !e.target.classList.contains("reserved")
+  ) {
+    e.target.classList.toggle("selected");
+    calculeteTotal();
+  }
+});
+
+select.addEventListener("change", function (e) {
+  calculeteTotal();
+});
+
+function calculeteTotal() {
+  const selectedSeats = container.querySelectorAll(".seat.selected");
+
+  const selectedSeatsArr = [];
+  const seatsArr = [];
+
+  selectedSeats.forEach(function (seat) {
+    selectedSeatsArr.push(seat);
+  });
+
+  seats.forEach(function (seat) {
+    seatsArr.push(seat);
+  });
+
+  let selectedSeatIndexs = selectedSeatsArr.map(function (seat) {
+    return seatsArr.indexOf(seat);
+  });
+
+  let selectedSeatCount = selectedSeats.length;
+  count.innerText = selectedSeatCount;
+  amount.innerText = selectedSeatCount * select.value;
+
+  saveToLocalStorage(selectedSeatIndexs);
+}
+function getFromLocalStorage() {
+  const selectedSeats = JSON.parse(localStorage.getItem("selectedSeats"));
+
+  if (selectedSeats != null && selectedSeats.length > 0) {
+    seats.forEach(function (seat, index) {
+      if (selectedSeats.indexOf(index) > -1) {
+        seat.classList.add("selected");
+      }
+    });
+  }
+
+  const selectedMovieIndex = localStorage.getItem("selectedMovieIndex");
+  if (selectedMovieIndex != null) {
+    select.selectedIndex = selectedMovieIndex;
+  }
+}
+
+function saveToLocalStorage(indexs) {
+  localStorage.setItem("selectedSeats", JSON.stringify(indexs));
+  localStorage.getItem("selectedMovieIndex", select.selectedIndex);
+}
